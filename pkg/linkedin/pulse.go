42b279fd8567e1b9b1ea8a80e6ff96cf5c2f3d7a
Bart Van Bos <bart@tetrate.io>
2023-10-02 06:42:56 +0200

    added posts cmd

--- pkg/linkedin/pulse.go
+++ pkg/linkedin/pulse.go
@@ -4,12 +4,14 @@ import "time"
 
 // Pulse represents the structure of a LinkedIn pulse.
 type Pulse struct {
-	Author      int      `json:"author"`
-	AuthorTitle string   `json:"authorTitle"`
-	HashTags    []string `json:"hashTags"`
-	LikesCount  int      `json:"likesCount"`
-	PublishDate string   `json:"publishDate"`
-	Title       string   `json:"title"`
+	Author       int      `json:"author"`
+	AuthorTitle  string   `json:"authorTitle"`
+	CommentCount int      `json:"commmentCount"`
+	HashTags     []string `json:"hashTags"`
+	LikesCount   int      `json:"likesCount"`
+	PublishDate  string   `json:"publishDate"`
+	PulseLink    string   `json:"pulseLink"`
+	Title        string   `json:"title"`
 }
 
 func SearchPulsesOnline(keywords []string, interval time.Duration, debug bool) ([]Pulse, error) {
